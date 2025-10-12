060aa0012cf7c554935d5c16f26792e3f5e5ef0c
Matt McAnear <mmcanear@umich.edu>
2025-10-12 15:30:31 -0400

    Add documentation to the data generating process

--- src/dgps.py
+++ src/dgps.py
@@ -19,13 +19,39 @@ def create_covariance_matrix(n, rho=0.0):
     Create an n x n covariance matrix with 1s on the diagonal and rho elsewhere.
     """
     C = np.ones((n, n)) * rho
-    # huh, so this modifies in place for some reason
+    # This modifies in place for some reason
     np.fill_diagonal(C, 1)
     return C
 
 
 @dataclass
 class SimulatedData(object):
+    """
+    Container for simulated data and parameters.
+    Also includes methods for saving and loading from disk.
+    
+    Attributes
+    ----------
+    X : np.ndarray
+        Design matrix of shape (N, n).
+    y : np.ndarray
+        Binary response vector of shape (N,).
+    true_beta : np.ndarray
+        True regression coefficients of shape (n,).
+    n : int
+        See DatasetGenerator.
+    rho : float
+        See DatasetGenerator.
+    tau_0 : float
+        See DatasetGenerator.
+    tau_1 : float
+        See DatasetGenerator.
+    sigma2 : float
+        See DatasetGenerator.
+    rng : np.random.Generator
+        Random number generator used for reproducibility.
+    """
+    
     X: np.ndarray
     y: np.ndarray
     true_beta: np.ndarray
@@ -36,14 +62,23 @@ class SimulatedData(object):
     sigma2: float
     rng: np.random.Generator
 
+    def __post_init__(self):
+        self.N = self.X.shape[0]
+
     def __repr__(self):
+        """
+        More detailed representation for debugging.
+        """
         return (
-            f"SimulatedData(n={self.n}, rho={self.rho}, tau_0={self.tau_0}, "
+            f"<SimulatedData(N={self.N}, n={self.n}, rho={self.rho}, tau_0={self.tau_0}, "
             f"tau_1={self.tau_1}, sigma2={self.sigma2}, "
-            f"X_shape={self.X.shape}, y_shape={self.y.shape})"
+            f"X_shape={self.X.shape}, y_shape={self.y.shape})>"
         )
 
     def __iter__(self):
+        """
+        Allow unpacking like a tuple: X, y, true_beta = simulated_data
+        """
         yield from (self.X, self.y, self.true_beta)
 
     def __str__(self) -> str:
@@ -51,11 +86,22 @@ class SimulatedData(object):
 
     def __create_filename(self):
         return Path(
-            f"simdata_n{self.n}_rho={self.rho}_tau0={self.tau_0}_"
+            f"simdata_N{self.N}n{self.n}_rho={self.rho}_tau0={self.tau_0}_"
             f"tau1={self.tau_1}_sigma2={self.sigma2}.pkl"
         )
 
     def save(self, output_dir: Union[Path, str]):
+        """
+        Save the simulated data for later, along with RNG state for 
+        reproducibility.
+
+        Parameters
+        ----------
+        output_dir : Union[Path, str]
+            Directory to save the data file in. Note that you provide a 
+            directory because the filename is decided based on the parameters
+            used in the generation of the data.
+        """
         output_file = Path(output_dir) / self.__create_filename()
         output_file.parent.mkdir(parents=True, exist_ok=True)
         logging.debug(f"Saving simulated data to {output_file}")
@@ -66,6 +112,20 @@ class SimulatedData(object):
 
     @classmethod
     def load(cls, filepath):
+        """
+        Load simulated data from disk. Note that this can be used to load
+        any pickled object, so we check that the loaded object is of the
+        correct type.
+
+        Parameters
+        ----------
+        filepath : Union[Path, str]
+            Path to the saved data file.
+
+        Returns
+        -------
+        SimulatedData
+        """
         with open(filepath, "rb") as f:
             data = pkl.load(f)
         assert isinstance(data, cls), f"Loaded data is not of type {cls.__name__}"
@@ -74,6 +134,34 @@ class SimulatedData(object):
 
 class DatasetGenerator(object):
     def __init__(self, n=5, rho=0.0, tau_0=1.0, tau_1=1.0, sigma2=1.0, rng=None):
+        """
+        Constructor class for generating simulated datasets with the given 
+        parameters. This class utilizes a callable interface, so when generating
+        from this class, you use it like a function (e.g. `generate_data(N)`),
+        where N is the number of samples to generate.
+        
+        Parameters
+        ----------
+        n : int
+            Number of predictors.
+        rho : float
+            Correlation parameter used in covariance matrix generation of Z 
+            values. Combined with draws from a uniform (-0.25, 0.25) uniform
+            distribution to create correlated binary predictors in X, 
+            representing exposures.
+        tau_0 : float
+            Standard deviation for noise (δ_i) in true beta generation.
+        tau_1 : float
+            Scale parameter for the exponential distribution in beta generation,
+            the actual beta effect. For any given beta, there is an 
+            80% change that this simulated value is ignored and assigned a 0.
+        sigma2 : float
+            Standard deviation of the noise (ϵ_k) added to the logits for 
+            individual rows, i.e. additional noise in the log-odds not accounted
+            for by the binomial distribution assumed on Y conditional on X.
+        rng : np.random.Generator
+            Random number generator used for reproducibility.
+        """
         if rng is None:
             rng = np.random.default_rng(rng)
         self.n = n
@@ -100,6 +188,14 @@ class DatasetGenerator(object):
         )
 
     def generate_design_matrix(self, N, true_beta):
+        """
+        Generate the design matrix X and binary response vector y as outlined
+        in Greenland (1993). The design matrix X is a binary matrix of u
+        exposures, generated by thresholding correlated normal variables Z. 
+        Additional noise is added over the standard binomial variance for
+        y | X. Finally, the alpha intercept is chosen to center the logits
+        and ensure that roughly 50% of the responses are 1s.
+        """
         logger.debug(f"Using RNG: {self.rng}")
 
         Z = self.rng.multivariate_normal(
@@ -127,6 +223,11 @@ class DatasetGenerator(object):
         return X, y
 
     def generate_beta(self):
+        """
+        Generate true regression coefficients for simulation. Interestingly,
+        the beta draws are independent, despite us assuming correlation
+        when estimated a parameter shrinkage term.
+        """
         z_i = self.rng.choice([0, 1], size=self.n, p=[0.8, 0.2])
         pi_i = self.rng.exponential(self.tau_1, size=self.n)
 
