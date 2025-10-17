15197d5e6c0dbd7b7b67f964af0f88adbb4b79c3
Matt McAnear <mmcanear@umich.edu>
2025-10-17 15:15:05 -0400

    simplify simulation interface

--- src/simulation.py
+++ src/simulation.py
@@ -1,6 +1,7 @@
 import logging
 import xarray as xr
 import numpy as np
+import inspect
 
 from src.dgps import DatasetGenerator
 from src.methods import fit_mle, fit_parametricEB, fit_semiBayes
@@ -8,59 +9,77 @@ from src.methods import fit_mle, fit_parametricEB, fit_semiBayes
 logger = logging.getLogger(__name__)
 
 
-class SimulationResult(object):
-    pass
+def get_sim_args(func, sim_args: dict, prepend=None):
+    signature = inspect.signature(func)
+    prepend = prepend + "_" if prepend else ""
+    default_args = {
+        f"{prepend + k}": v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect._empty
+    }
+
+    # override any default arguments with provided arguments
+    sim_args_complete = {**default_args, **sim_args}
+    return sim_args_complete
 
 
 def run_simulation(
+    N_sim,
     data_generation_fn: DatasetGenerator,
     sbParams=None,
     ebParams=None,
     **generation_kwargs,
 ):
-    # get passed in dict values or initialize empty dicts
-    sbParams = sbParams or {}
-    ebParams = ebParams or {}
+    # get default from fitting functions if this is an empty dict
+    sbParams = get_sim_args(fit_semiBayes, sbParams or {})
+    ebParams = get_sim_args(fit_parametricEB, ebParams or {})
 
-    X, y, beta = data_generation_fn(**generation_kwargs)
-    mle = fit_mle(X, y)
-    parametric_eb = fit_parametricEB(mle, **ebParams)
-    semi_bayes = fit_semiBayes(mle, **sbParams)
+    def _run_simulation():
+        # get passed in dict values or initialize empty dicts
+        X, y, beta = data_generation_fn(**generation_kwargs)
 
-    mle_beta, mle_cov = mle.params, np.diagonal(mle.cov_params())
-    pb_beta, pb_cov, _ = parametric_eb
-    sb_beta, sb_cov = semi_bayes
+        mle = fit_mle(X, y)
+        parametric_eb = fit_parametricEB(mle, **ebParams)
+        semi_bayes = fit_semiBayes(mle, **sbParams)
 
-    beta_estimates = np.stack(
-        [
-            np.stack([mle_beta, pb_beta, sb_beta]),
-            np.stack(
-                [
-                    np.sqrt(mle_cov),
-                    np.sqrt(np.diagonal(pb_cov)),
-                    np.sqrt(np.diagonal(sb_cov)),
-                ]
-            ),
-        ]
-    )
+        mle_beta, mle_cov = mle.params, np.diagonal(mle.cov_params())
+        pb_beta, pb_cov, _ = parametric_eb
+        sb_beta, sb_cov = semi_bayes
 
-    sim_estimates = xr.DataArray(
-        beta_estimates,
-        dims=["var", "method", "param"],
-        coords={
-            "var": ["estimate", "std_error"],
-            "method": ["mle", "parametric_eb", "semi_bayes"],
-            "param": [f"beta{i + 1}" for i in range(X.shape[1])],
-        },
-    ).transpose("method", "param", "var")
+        beta_estimates = np.stack(
+            [
+                np.stack([mle_beta, pb_beta, sb_beta]),
+                np.stack(
+                    [
+                        np.sqrt(mle_cov),
+                        np.sqrt(np.diagonal(pb_cov)),
+                        np.sqrt(np.diagonal(sb_cov)),
+                    ]
+                ),
+            ]
+        )
 
-    sim_estimates.attrs = {
-        "true_beta": beta,
+        sim_estimates = xr.Dataset(
+            {
+                "beta_hat": (("var", "method", "param"), beta_estimates),
+                "true_beta": (("param",), beta),
+            },
+            coords={
+                "var": ["estimate", "std_error"],
+                "method": ["mle", "parametric_eb", "semi_bayes"],
+                "param": [f"beta{i + 1}" for i in range(X.shape[1])],
+            },
+        )
+
+        logging.debug(f"simulation complete -- {data_generation_fn.__dict__}")
+        return sim_estimates
+
+    sim_output = xr.concat([_run_simulation() for _ in range(N_sim)], dim="simulation")
+    sim_output.attrs = {
+        "N": N_sim,
+        **{f"sb_{k}": v for k, v in sbParams.items()},
+        **{f"eb_{k}": v for k, v in ebParams.items()},
         **generation_kwargs,
         **data_generation_fn.__dict__,
-        "sbParams": sbParams,
-        "ebParams": ebParams,
     }
-    logging.debug(f"simulation complete -- {sim_estimates.attrs}")
-
-    return sim_estimates
+    return sim_output
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -2,13 +2,15 @@ from src.simulation import run_simulation
 
 
 def test_simulation(generate_data):
-    out = run_simulation(generate_data)
-    assert out.dims == ("method", "param", "var")
+    out = run_simulation(100, generate_data)
+
+    assert not set(out["beta_hat"].dims).difference(
+        {"simulation", "method", "param", "var"}
+    )
     assert set(out.coords["method"].values) == {"mle", "parametric_eb", "semi_bayes"}
-    assert out.shape[0] == 3  # methods
 
 
 def test_simulation_output(generate_data):
-    result = run_simulation(generate_data, N=50)
+    result = run_simulation(50, generate_data)
     assert result.attrs["N"] == 50
     assert not set(generate_data.__dict__.keys()).difference(result.attrs.keys())
