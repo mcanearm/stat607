b82e0e55ab6acdfea2e88508bdd556f3b8f38e04
Matt McAnear <mmcanear@umich.edu>
2025-11-11 15:02:01 -0500

    update timing script to follow API of later simulations (scenarios differ)

--- scripts/record_timings.py
+++ scripts/record_timings.py
@@ -4,6 +4,8 @@ import os
 import pandas as pd
 from src.utils import time_fn, get_git_hash
 from pathlib import Path
+import argparse
+import numpy as np
 
 
 logger = logging.getLogger(__name__)
@@ -11,24 +13,53 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
 
 if __name__ == "__main__":
-    n_sims = [100, 500, 1000, 2000]
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output-dir",
+        type=str,
+        default="./results/timings/",
+        help="Directory to save timing results",
+    )
+    parser.add_argument(
+        "--num-cores",
+        type=int,
+        default=1,
+        help="Number of CPU cores to use in each scenario (within job parallelism)",
+    )
+    parser.add_argument(
+        "--tag",
+        type=str,
+        default=None,
+        help="Tag for the output filename. Defaults to the Git hash of the current code version.",
+    )
+    args = parser.parse_args()
 
-    timings = [
+    n_sims = [100, 500, 1000, 2000]
+    file_tag = args.tag or get_git_hash()
+
+    seed = 20250607
+    rng = np.random.default_rng(seed)
+    new_rngs = rng.spawn(len(scenarios) * len(n_sims))
+
+    scenarios = [
         (
             scenario[0],
             scenario[1],
             n_sim,
-            time_fn(run_scenario)(scenario, N_sim=n_sim)[1],
         )
         for scenario in scenarios
         for n_sim in n_sims
     ]
 
+    timings = [
+        (*scenario, time_fn(run_scenario)((new_rngs[i], args.num_cores, *scenario))[1])
+        for i, scenario in enumerate(scenarios)
+    ]
+
+    # time_fn(run_scenario)(scenario)[1],
     timing_df = pd.DataFrame(
         timings, columns=["n", "N", "n_sim", "time_seconds"]
     ).set_index(["n", "N", "n_sim"])
 
-    git_hash = get_git_hash()
-
     (output_dir := Path("./results/timings/")).mkdir(parents=True, exist_ok=True)
-    timing_df.to_csv(output_dir / f"simulation_timings_{git_hash}.csv")
+    timing_df.to_csv(output_dir / f"simulation_timings_{file_tag}.csv")
