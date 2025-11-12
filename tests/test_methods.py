3d7dc9435640144a00f5c112bf08371cbb372854
Matt McAnear <mmcanear@umich.edu>
2025-11-12 18:25:16 -0500

    revert MLE optimization based on timings comparison

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -52,9 +52,10 @@ def test_parametricEB(mle_model):
 @pytest.mark.parametrize("tau2", [0.5, 1.0, 10.0, 1000], ids=lambda t: f"tau2={t}")
 def test_semi_bayes(mle_model, tau2):
     beta_star, _ = fit_semiBayes(mle_model, tau2)
+    beta_mle, _ = __get_mle_vhat(mle_model)
 
     # assert shrinkage occurs, at least when tau2 is not TOO small
-    assert np.linalg.norm(beta_star) <= np.linalg.norm(mle_model[0])
+    assert np.linalg.norm(beta_star) <= np.linalg.norm(beta_mle)
 
 
 def test_numerical_stability_catch():
